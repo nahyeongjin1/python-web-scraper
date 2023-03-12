818289ad8c22cf7b65ff6c0f19cb5484e9157d90
나형진 <skgudwls@konkuk.ac.kr>
2023-03-12 06:57:40 +0000

    Initial request and add library called beautifulsoup

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from requests import get
 
-websites = [
-  "google.com", "twitter.com", "naver.com", "youtube.com", "instagram.com",
-  "nomadcoders.co"
-]
-results = {}
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
 
-for website in websites:
-  if not website.startswith("https://"):
-    website = f"https://{website}"
-  response = get(website)
-  if response.status_code == 200:
-    results[website] = "OK"
-  else:
-    results[website] = "FAILED"
-
-print(results)
+response = get(f"{base_url}{search_term}")
+if not response.status_code == 200:
+  print("Can't request website :(")
+else:
+  print(response.text)
